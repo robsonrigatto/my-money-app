5e9c36fad5e475d746e2017da6cfd6bb0fea0717
robsonrigatto <rigatto.robson@gmail.com>
2020-06-20 04:28:39 -0300

    initial frontend structure

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright Robson Rigatto: 2020
+            <a href='http://github.com/robsonrigatto' target='_blank'> Github</a>
+        </strong>
+    </footer>
+)
