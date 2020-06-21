b57b81089e1a5a5dcc40ef12e29de629aab3244b
robsonrigatto <rigatto.robson@gmail.com>
2020-06-21 17:53:01 -0300

    creating billingcycle

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 import {toastr} from 'react-redux-toastr'
-import {reset as resetForm} from 'redux-form'
-import {showTabs, selectTabs, selectTab} from '../common/tab/tabActions'
+import {reset as resetForm, initialize} from 'redux-form'
+import {showTabs, selectTab} from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
+
+const INITIAL_VALUES = {}
 
 export function getList() {
     const request = axios.get(`${BASE_URL}/billingCycles`)
@@ -18,12 +20,7 @@ export function create(values) {
         const request = axios.post(`${BASE_URL}/billingCycles`, values)
         .then(resp => {
             toastr.success('Sucesso', 'Operação realizada com sucesso')
-            dispatch([
-                resetForm('billingCycleForm'),
-                getList(),
-                selectTab('tabList'),
-                showTabs('tabList', 'tabCreate')
-            ])
+            dispatch(init())
         })
         .catch(e => {
             e.response.data.errors.forEach(error => {
@@ -31,4 +28,21 @@ export function create(values) {
             });
         })
     }
+}
+
+export function showUpdate(bc) {
+    return [
+        showTabs('tabUpdate'),
+        selectTab('tabUpdate'),
+        initialize('billingCycleForm', bc)
+    ]
+}
+
+export function init() {
+    return [
+        showTabs('tabList', 'tabCreate'),
+        selectTab('tabList'),
+        getList(),
+        initialize('billingCycleForm', INITIAL_VALUES)
+    ]
 }
--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -3,7 +3,7 @@ import React, {Component}  from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
-import {getList} from './billingCycleActions'
+import {getList, showUpdate} from './billingCycleActions'
 
 class BillingCycleList extends Component {
 
@@ -19,6 +19,11 @@ class BillingCycleList extends Component {
                 <td>{bc.name}</td>
                 <td>{bc.month}</td>
                 <td>{bc.year}</td>
+                <td>
+                    <button className='btn btn-warning' onClick={() => this.props.showUpdate(bc)}>
+                        <i className='fa fa-pencil'></i>
+                    </button>
+                </td>
             </tr>
         ))
     }
@@ -32,6 +37,7 @@ class BillingCycleList extends Component {
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
+                            <th>Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -44,5 +50,5 @@ class BillingCycleList extends Component {
 }
 
 const mapStateToProps = state => ({list: state.billingCycle.list})
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate}, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
